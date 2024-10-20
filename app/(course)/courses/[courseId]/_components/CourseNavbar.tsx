4df0b082d3c8755804592a4bb90109c99c179c86
Slavcho <s.trajkovski02@yahoo.com>
2024-10-20 14:47:21 +0200

    attempt to fix hydration error

--- app/(course)/courses/[courseId]/_components/CourseNavbar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseNavbar.tsx
@@ -1,6 +1,6 @@
 import NavbarRoutes from "@/components/NavbarRoutes";
 import { Chapter, Course, UserProgress } from "@prisma/client";
-import React from "react";
+import React, { Suspense } from "react";
 import CourseMobileSidebar from "./CourseMobileSidebar";
 
 interface CourseNavbarProps {
@@ -15,10 +15,12 @@ interface CourseNavbarProps {
 const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-        <CourseMobileSidebar course={course} progressCount={progressCount}/>
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
+      <Suspense>
         <NavbarRoutes />
+      </Suspense>
     </div>
-  )
+  );
 };
 
 export default CourseNavbar;
