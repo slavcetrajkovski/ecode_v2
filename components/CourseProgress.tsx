887f7dfa1823bb6e214fd2ddcbca4b0c738e18a9
Slavcho <s.trajkovski02@yahoo.com>
2024-10-21 13:39:02 +0200

    fix build errors

--- components/CourseProgress.tsx
+++ components/CourseProgress.tsx
@@ -3,22 +3,33 @@ import { Progress } from "./ui/progress";
 
 interface CourseProgressProps {
   value: number;
+  variant?: "default" | "success";
   size?: "default" | "sm";
 }
+
+const colorByVariant = {
+  default: "text-ecode_primary/90",
+  success: "text-emerald-700",
+};
 
 const sizeByVariant: any = {
   default: "text-sm",
   success: "text-xs",
 };
 
-export const CourseProgress = ({ value, size }: CourseProgressProps) => {
+export const CourseProgress = ({
+  value,
+  variant,
+  size,
+}: CourseProgressProps) => {
   return (
     <div>
-      <Progress className="h-2" value={value} />
+      <Progress className="h-2" value={value} variant={variant} />
       <p
         className={cn(
-          "font-medium mt-2 text-emerald-700",
-          sizeByVariant[size || "default"]
+          "font-medium mt-2 text-sky-700",
+          colorByVariant[variant || "success"],
+          sizeByVariant[size || "success"]
         )}
       >
         {Math.round(value)}% Complete
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -4,13 +4,31 @@ import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const progressClass = "h-full w-full flex-1 bg-emerald-700 transition-all";
+const progressVariants = cva("h-full w-full flex-1 bg-primary transition-all", {
+  variants: {
+    variant: {
+      default: "bg-ecode_primary",
+      success: "bg-emerald-700",
+    },
+  },
+  defaultVariants: {
+    variant: "success",
+  },
+});
+
+export interface ProgressProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof progressVariants> {}
+
+type CombinedProgressProps = ProgressProps &
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>;
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  CombinedProgressProps
+>(({ className, value, variant, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -20,7 +38,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={progressClass}
+      className={cn(progressVariants({ variant }))}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
